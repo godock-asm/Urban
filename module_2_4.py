10ec893d3d80052745d23d18e5b643dd3d44b18a
godock-asm <arzsergey@gmail.com>
2024-12-11 03:34:15 +0300

    first commit

--- module_2_4.py
+++ module_2_4.py
@@ -30,4 +30,35 @@ for n in numbers:
 
 
 
+#Домашнее задание по теме "Цикл While"
+#Цель: закрепить навык решения задач при помощи цикла for, применив знания об основных типах данных.
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = [ 2, 3, 5, 7, 11, 13]
+print(*primes)
+not_primes = [4, 6, 8, 9, 10, 12, 14, 15]
+print(*not_primes)
+
+
+primes = []
+not_primes = []
+n=1
+while numbers<=[15]:
+        if numbers > [1]:
+            is_prime = True
+        range(2, 15)
+while numbers < [16]:
+            if numbers % n == 0:
+                is_prime = False
+            range(2, 16)
+            break
+if is_prime:
+    primes.append(n)
+else:
+    not_primes.append(n)
+print(f'Primes: {primes}')
+print(f'Not Primes: {not_primes}')
+
+
 
